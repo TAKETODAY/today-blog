aa8e3b29a5237d8f7a04235eb39b1434efc7df6f
Harry Yang <i.take.today@gmail.com>
2023-03-26 20:40:21 +0800

    admin Statistics

--- blog-frontend/admin/src/pages/workplace/components/Statistics/index.tsx
+++ blog-frontend/admin/src/pages/workplace/components/Statistics/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+/*
+ * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
+ * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ */
+
+import React, { useEffect, useState } from 'react';
 import FieldStatistics from "../FieldStatistics";
 import PageViewStatistics from "../PageViewStatistics";
 import { getCacheable } from "@/utils";
 
 type StatisticsProps = {}
 
-const statistics = [
+const statisticsMap = [
   {
     api: '/api/statistics/os',
     name: { x: '操作系统种类', y: '数量', series: '数量' },
@@ -23,14 +43,17 @@ const statistics = [
   },
 ]
 
-statistics.forEach((stat) => {
-  getCacheable(stat.api).then(res => {
-    // @ts-ignore
-    stat.data = res.data
-  })
-})
-
 const Statistics: React.FC<StatisticsProps> = (props) => {
+  const [statistics, setStatistics] = useState([])
+  useEffect(() => {
+    statisticsMap.forEach((stat) => {
+      getCacheable(stat.api).then(res => {
+        // @ts-ignore
+        stat.data = res.data
+      })
+    })
+    setStatistics(statisticsMap)
+  }, [])
 
   return <>
     {
@@ -39,7 +62,7 @@ const Statistics: React.FC<StatisticsProps> = (props) => {
       })
     }
 
-    <PageViewStatistics />
+    <PageViewStatistics/>
   </>
 }
 
