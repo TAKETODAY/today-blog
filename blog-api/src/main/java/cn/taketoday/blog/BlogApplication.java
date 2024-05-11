362917a891ccfddc0ce2330e0b39d2bcb4accf98
Harry Yang <i.take.today@gmail.com>
2024-05-11 21:02:29 +0800

    :art:

--- blog-api/src/main/java/cn/taketoday/blog/BlogApplication.java
+++ blog-api/src/main/java/cn/taketoday/blog/BlogApplication.java
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 package cn.taketoday.blog;
@@ -29,7 +26,7 @@ import lombok.CustomLog;
  * @since 2019-01-12 21:01
  */
 @CustomLog
-@InfraApplication
+@InfraApplication(proxyBeanMethods = false)
 public class BlogApplication {
 
   public static void main(String[] args) {
