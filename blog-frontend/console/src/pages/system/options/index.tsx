b2621470b218e082331c4a952876e5516036fa9d
海子 Yang <i.take.today@gmail.com>
2024-11-15 21:46:19 +0800

    :art:

--- blog-frontend/console/src/pages/system/options/index.tsx
+++ blog-frontend/console/src/pages/system/options/index.tsx
@@ -1,8 +1,5 @@
 /*
- * Original Author -> Harry Yang (taketoday@foxmail.com) https://taketoday.cn
- * Copyright © TODAY & 2017 - 2023 All Rights Reserved.
- *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER
+ * Copyright 2017 - 2024 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see [http://www.gnu.org/licenses/]
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
  */
 
 import { useEffect, useState } from 'react';
@@ -51,7 +48,7 @@ export default () => {
       }
     })
     if (Object.keys(formValues).length > 0) {
-      const hide = message.loading(`正在删除更新`)
+      const hide = message.loading(`正在更新`)
       try {
         await doUpdateOptions(formValues)
         message.success('更新成功，即将刷新', 1)
