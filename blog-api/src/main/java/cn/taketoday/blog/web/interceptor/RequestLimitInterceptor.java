e30fbfc724a691355c9df1e3503fc5c41b34f105
海子 Yang <i.take.today@gmail.com>
2026-03-28 21:13:39 +0800

    :arrow_up: API update

--- blog-api/src/main/java/cn/taketoday/blog/web/interceptor/RequestLimitInterceptor.java
+++ blog-api/src/main/java/cn/taketoday/blog/web/interceptor/RequestLimitInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2025 the original author or authors.
+ * Copyright 2017 - 2026 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -159,8 +159,8 @@ final class RequestLimitInterceptor implements HandlerInterceptor {
   }
 
   private static RequestLimit findRequestLimit(HandlerMethod handlerMethod) {
-    if (handlerMethod.hasMethodAnnotation(RequestLimit.class)) {
-      return handlerMethod.getMethodAnnotation(RequestLimit.class);
+    if (handlerMethod.isAnnotationPresent(RequestLimit.class)) {
+      return handlerMethod.getAnnotation(RequestLimit.class);
     }
     return handlerMethod.getBeanType().getAnnotation(RequestLimit.class);
   }
