23fefa8d9efff4f25fd0f01da327958a679a89ed
海子 Yang <i.take.today@gmail.com>
2026-04-01 20:32:41 +0800

    :art: UserType

---
+++ blog-api/src/main/java/cn/taketoday/blog/model/enums/UserType.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 - 2026 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [https://www.gnu.org/licenses/]
+ */
+
+package cn.taketoday.blog.model.enums;
+
+/**
+ * @author <a href="https://github.com/TAKETODAY">Harry Yang</a>
+ * @since 3.3 2026/4/1 20:28
+ */
+public enum UserType {
+
+  blogger,
+
+  normal,
+
+  github
+}
