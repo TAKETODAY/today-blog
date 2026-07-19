4f08432498946a2ceb7dfd98c095b4a7e2a353bb
海子 Yang <i.take.today@gmail.com>
2026-07-19 22:18:21 +0800

    :art: 优化 SiteMap 渲染
    加入图片

--- blog-api/src/main/java/cn/taketoday/blog/model/Sitemap.java
+++ blog-api/src/main/java/cn/taketoday/blog/model/Sitemap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 - 2025 the original author or authors.
+ * Copyright 2017 - 2026 the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -67,11 +67,12 @@ public class Sitemap implements Serializable {
     }
 
     String lastModify = updateAt.toString();
-    return newURL(0.9f, "/articles/" + article.getUri(), lastModify, "daily");
+    String cover = article.getCover();
+    return newURL(0.9f, "/articles/" + article.getUri(), lastModify, "daily", cover);
   }
 
-  public static SiteURL newURL(float priority, String location, String lastModify, String changeFreq) {
-    return new SiteURL(location, priority, lastModify, changeFreq);
+  public static SiteURL newURL(float priority, String location, String lastModify, String changeFreq, String cover) {
+    return new SiteURL(location, priority, lastModify, changeFreq, cover);
   }
 
   @Getter
@@ -86,11 +87,18 @@ public class Sitemap implements Serializable {
 
     private final String changeFreq;// 更新频率
 
-    public SiteURL(String loc, float priority, @Nullable String lastModify, String changeFreq) {
+    private final @Nullable String cover;
+
+    public SiteURL(String loc, float priority, @Nullable String lastModify, String changeFreq, @Nullable String cover) {
       this.loc = loc;
       this.priority = priority;
       this.lastModify = lastModify;
       this.changeFreq = changeFreq;
+      this.cover = cover;
+    }
+
+    public boolean hasCover() {
+      return cover != null;
     }
   }
 
--- blog-api/src/main/resources/templates/sitemap.ftl
+++ blog-api/src/main/resources/templates/sitemap.ftl
@@ -2,8 +2,7 @@
 <#-- @ftlvariable name="opt" type="java.util.Map" -->
 <?xml version="1.0" encoding="utf-8"?>
 <?xml-stylesheet type='text/xsl' href='sitemap.xsl'?>
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url>
     <loc>${opt['site.host']}</loc>
     <changefreq>daily</changefreq>
@@ -16,6 +15,16 @@
     <lastmod>${url.lastModify}</lastmod>
     <changefreq>${url.changeFreq}</changefreq>
     <priority>${url.priority}</priority>
+
+  <#if url.hasCover()>
+    <image:image>
+      <#if url.cover?starts_with("http")>
+      <image:loc>${url.cover}</image:loc>
+      <#else >
+      <image:loc>${opt['site.cdn']}${url.cover}</image:loc>
+      </#if>
+    </image:image>
+  </#if>
   </url>
 </#list>
 </urlset>
